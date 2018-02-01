242be903e5cba8af63ce4d972a7cd54c78acbe60
rrodenbusch <rrodenbusch@gmail.com>
2018-02-01 14:33:32 -0600

    updated to work

--- PatchRoute.pl
+++ PatchRoute.pl
@@ -4,7 +4,7 @@ use warnings;
 my $device = $ARGV[0];
 
 die "Only configured for wlan0 or wlan1" unless ($device eq "wlan0") or ($device eq "wlan1");
-my $resp = `route -n >routes.txt`;
+my $resp = system("route -n >routes.txt");
 open (my $fh, "routes.txt") or die "Unable to open routes.txt \n$!\n";
 my $line = <$fh>;
 $line  = <$fh>;
@@ -12,9 +12,12 @@ $line = <$fh>;
 $line =~ s/\R//g;
 my @flds = split(' ',$line);
 if ($flds[7] ne $device) {
-   $resp = `sudo route add default gw 192.168.0.1 dev $device`;
+   print "Updating route\n";
+   $resp = system("sudo route add default gw 192.168.0.1 dev $device");
+} else {
+   print "Route okay\n";
 }
 
-print "Done";
+print "Route Done\n";
 1;
 
