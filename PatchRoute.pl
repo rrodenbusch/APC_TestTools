cfca4bc82dbf1108a9fd2baa899580a132a427c3
rrodenbusch <rrodenbusch@gmail.com>
2018-02-01 14:51:09 -0600

    added correct file handling

--- PatchRoute.pl
+++ PatchRoute.pl
@@ -4,7 +4,7 @@ use warnings;
 my $device = $ARGV[0];
 
 die "Only configured for wlan0 or wlan1" unless ($device eq "wlan0") or ($device eq "wlan1");
-my $resp = system("route -n >routes.txt");
+my $resp = system("sudo route -n >routes.txt");
 open (my $fh, "routes.txt") or die "Unable to open routes.txt \n$!\n";
 my $line = <$fh>;
 $line  = <$fh>;
--- PatchRoute.sh
+++ PatchRoute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/APC_TestTools
 rm routes.txt
-./PatchRoute.pl $1
+/home/pi/APC_TestTools/PatchRoute.pl $1
