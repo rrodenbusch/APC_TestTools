75eb34248dbd0e948559687687821ed6863faa08
rrodenbusch <rrodenbusch@gmail.com>
2018-05-03 12:19:56 -0500

    change ping

--- checkWifi.pl
+++ checkWifi.pl
@@ -2,12 +2,16 @@
 use strict;
 use warnings;
 
-use Net::Ping;
+pi_ping{
+	my $host = shift;
+	my $cnt = `sudo ping -c 1 $host |grep icmp |wc -l`;
+	return($cnt);
+}
 
-my $p = Net::Ping->new();
+print "Google here\n" if pi_ping("8.8.8.8");
+print "yahoo here\n" if pi_ping("yahoo.com");
+print "vpn here\n" if pi_ping("10.50.0.1");
 
-print "Google here" if $p->ping("8.8.8.8");
-print "VPN here" if $p->ping("10.50.0.1");
-
+print "test\n" if pi_ping("8.8.8.2"); 
 
 1;
