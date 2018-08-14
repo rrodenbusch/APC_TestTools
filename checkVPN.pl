fb009873a1f281d2af9fc043b3e441e5da06f1e1
rrodenbusch <rrodenbusch@gmail.com>
2018-08-14 13:42:53 -0500

    Added IPP map file

--- checkVPN.pl
+++ checkVPN.pl
@@ -7,6 +7,9 @@ my $targCoach = $ARGV[0];
 my $FleetFile = "$ENV{HOME}/bin6/FleetDefinition.csv";
 my $ipFile = "$ENV{HOME}/git/APC_TestTools/ipp.txt";
 warn "Usage:  checkVPN.pl  coach#\n" unless defined($targCoach);
+my %Devices = ('NUC1'=>1,'NUC2'=>1,'VB1'=>1,'VB2'=>1,
+					'Pi1-End1'=>1,'Pi2-End1'=>1,'Pi1-End2'=>1,'Pi2-End2'=>1,
+					'NVR1'=>1,'NVR2'=>1);
 
 my $p = Net::Ping->new();
 if ($p->ping("10.50.0.1")) {
@@ -16,6 +19,16 @@ if ($p->ping("10.50.0.1")) {
 	print "\nVPN OFFLINE\n";
 	exit 0;
 }
+
+my %ipMap;
+if (open( my $infh, $ipFile)) {
+	while (my $line = <$infh>) {
+		$line =~ s/\R//g;
+		my @flds = split(',',$line);
+		$ipMap{$flds[0]} = $flds[1];
+	}
+}
+
 print "   Checking coach $targCoach\t";
 open(my $fh, "$FleetFile") or die "Unable to open $FleetFile\n";
 my $header = <$fh>;
@@ -35,27 +48,43 @@ while (my $line = <$fh>) {
 			my @parts = split(' ',$names[$i]);
 			my $last = pop(@parts);
 			if ($last eq 'VPN') {
-				if ( defined($fields[$i]) ) {
-					$names[$i] = "OtherIP" unless defined($names[$i]);
-					print "\t$names[$i]:\t$fields[$i]";
-					my $ip = $fields[$i];
-					$ip =~ s/ //g;
-					if ( ($ip ne '') ) {
-						if ($p->ping($fields[$i]) ) {
-							print "\tALIVE\n";
-						} else {
-							print "\tOffline\n";
-						}									
-					} else {
+#				if ( defined($fields[$i]) ) {
+#					$names[$i] = "OtherIP" unless defined($names[$i]);
+#					print "\t$names[$i]:\t$fields[$i]";
+#					my $ip = $fields[$i];
+#					$ip =~ s/ //g;
+#					if ( ($ip ne '') ) {
+#						if ($p->ping($fields[$i]) ) {
+#							print "\tALIVE\n";
+#						} else {
+#							print "\tOffline\n";
+#						}									
+#					} else {
+#						print "\n";
+#					}
+#				} else {
+#					print "\t$names[$i]:\tUndef\n";
+#				}
+			} elsif ( ($last eq 'MAC') ) {
+				if ($parts[0] eq 'CP') {
+					my ($p1,$p2,$p3) = (substr($fields[$i],6,2),substr($fields[$i],8,2),substr($fields[$i],10,2));
+					my $gpsID = hex($p1) . hex($p2) . hex($p3);
+					print "\tGPS ID\t$gpsID\n";
+				} elsif ($Devices{$parts[0]}) {
+					my $devMAC = $fields[$i];
+					my $devIP = $ipMap{$devMAC} if (defined($devMAC));
+					my $devName = $parts[0];
+					print "\t$devName";
+					print "\t" if (length($devName) < 8);
+					if ( !defined($devIP) || ($devIP eq '') ) {
+						print "\t$devMAC" if defined($devMAC);
 						print "\n";
-					}
-				} else {
-					print "\t$names[$i]:\tUndef\n";
+					} elsif ($p->ping($devIP) ) {
+						print "\t$devIP\tALIVE\n";
+					} else {
+						print "\t$devIP\tOffline\n";
+					}									
 				}
-			} elsif ( ($last eq 'MAC') && ($parts[0] eq 'CP') ) {
-				my ($p1,$p2,$p3) = (substr($fields[$i],6,2),substr($fields[$i],8,2),substr($fields[$i],10,2));
-				my $gpsID = hex($p1) . hex($p2) . hex($p3);
-				print "\tGPS ID\t$gpsID\n";
 			} 
 		}
 	}
