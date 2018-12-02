68a932d5b281489d7645a1ef66650c55a98d10ad
rrodenbusch <rrodenbusch@gmail.com>
2018-12-02 09:31:41 -0600

    Added git setup tools

---
+++ GitForce.sh
@@ -0,0 +1,8 @@
+git config core.editor nano
+git config user.email rrodenbusch@gmail.com
+git config user.name rrodenbusch
+git config --global credential.helper store
+git config --global push.default matching
+git fetch --all
+git reset --hard origin/master
+git pull origin master
---
+++ GitSetup.sh
@@ -0,0 +1,6 @@
+git config core.editor nano
+git config user.email rrodenbusch@gmail.com
+git config user.name rrodenbusch
+git config --global credential.helper store
+git config --global push.default matching
+
