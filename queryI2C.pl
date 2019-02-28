c53b1ee4782fa21eba42d822678cc145fc25123e
rrodenbusch <rrodenbusch@gmail.com>
2019-02-28 09:13:26 -0600

    fix

--- queryI2C.pl
+++ queryI2C.pl
@@ -101,8 +101,8 @@ sub queryI2Cbus {
       for (my $i = 0; $i<16; $i++) {
          if ($addy <= 0x7F) {
             my $curAddy = substr($line,$offset,2);
+            print "Chekcing $curAddy\n";
             if ( ($curAddy ne "  ") && ($curAddy ne "--")) {
-            print "Chekcing $curAddy\n");
                my $numAddy = hex("0x".$curAddy);
                $ValidI2C[$addy] = ($numAddy == $addy);
                $offset += $inc;
