fc8ec82acb4b64b26c1f5bd78fb1fac7a6a623a3
rrodenbusch <rrodenbusch@gmail.com>
2019-02-28 09:50:55 -0600

    fix

--- queryI2C.pl
+++ queryI2C.pl
@@ -130,13 +130,13 @@ queryI2Cbus(\@ValidI2C);
 #}
 
 if ($ValidI2C[0x68] ) { # check the MPU6050
-   print "Checking MPU\n";
+   print "Checking MPU\n"; 
    my $device = MPU6050->new(0x68);
    $device->wakeMPU(4);
    sleep(2);
 	my ($AcX,$AcY,$AcZ) = $device->readAccelG();
 	my ($tmp,$tmpC,$tmpF) = $device->readTemp();
-	my $line= sprintf( "MPU Data:: Ax: %4.2f Ay: %4.2f Az: %4.2f TempF %4.2tmpF\n", $AcX,$AcY,$AcZ,$tmpf);
+	my $line= sprintf( "MPU Data:: Ax: %4.2f Ay: %4.2f Az: %4.2f TempF %4.2tmpF\n", $AcX,$AcY,$AcZ,$tmpF);
 	print "$line\n";
 
 1;
