aade6fe8361f91c4e5a52e85fd4d924dc57c7b00
rrodenbusch <rrodenbusch@gmail.com>
2019-04-05 12:54:30 -0500

    fixed output

--- ArdIO.pl
+++ ArdIO.pl
@@ -5,6 +5,7 @@ use warnings;
 use lib "$ENV{HOME}/RPi";
 
 use RPi::I2C;
+my    $MAXTRIES = 50;
 
 
 sub attach {
@@ -36,8 +37,10 @@ sub get10Bit {
          $chkbyte ^= $n4;
       } else {
          $cnt++;
+         $chkbyte = 0xFF;
+         $word1 = 0xFFFF;
       }         
-   } while ( $chkbyte != 0);
+   } while ( ($cnt < $MAXTRIES) && ($chkbyte != 0) );
    return( $word1 & 0x3FF);
 }
 
@@ -53,30 +56,54 @@ sub getByte {
          $chksum = (~$chkbyte) & 0xFF;
       } else {
          $cnt++;
+         $databyte = 0xFF;
          $chksum = ~$databyte;
       }         
-   } while ( $databyte != $chksum );
+   } while ( ($databyte != $chksum ) && ($cnt < $MAXTRIES) );
    return( $databyte );
 }  # end getByte
+
+sub getDoors {
+   my ($addy) = @_;
+   my $cmd = 0x03;
+   my $doors = getByte($addy,$cmd);
+   my @doorStat = ( "UNKNOWN","UNKNOWN");
+   if ($doors <= 0x03) {
+      my $doorRight = $doors & 0x01;
+      my $doorLeft = ($doors & 0x02) >> 1;
+      #  Old FBD has pin high on open door1
+      $doorStat[0] = ($doorLeft == 0) ? "CLOSED" : "OPEN   ";
+      $doorStat[1] = ($doorRight == 0) ? "CLOSED" : "OPEN   ";
+   }
+   # Return Left / Right
+   return($doorStat[0],$doorStat[1]);
+}
+
+sub getVoltage {
+   my ($addy) = @_;
+   my $cmd = 0x0A;
+   my $voltage = getByte($addy,$cmd);
+   $voltage = $voltage / 10.0;
+   return ($voltage);
+}
 
 my $version = getByte(0x08,0x00);
 while (1) {
    # end of 
-   my $doors = getByte(0x08,0x03);
-   my $door1 = $doors & 0x01;
-   my $door2 = ($doors & 0x02) >> 1;
-   my $voltage = getByte(0x08,0x0A);
-   $voltage = $voltage / 10.0;
+   my ($doorLeft,$doorRight) = getDoors(0x08);
+   my $voltage = getVoltage(0x08);
+   
    my $tempCnt = getByte(0x08,0x12);
    my ($temp1, $temp2) = (-255.0,-255.0);
    if ($tempCnt > 0) {
       $temp1 = get10Bit(0x08,0x13);
       }
    if ($tempCnt > 1) {
-      $temp1 = get10Bit(0x08,0x14);
+      $temp2 = get10Bit(0x08,0x14);
       }
-   my $str = sprintf("%d %d %d %d %d %d %d\n", $version,$voltage,$door1,$door2,$temp1,$temp2);
+   my $str = sprintf("Version:%d %04.2fV Left:%d Right:%d Blower:%06.1f Intake%d %d\n", $version,$voltage,$doorLeft,$doorRight,$temp1,$temp2);
    print $str;
+   sleep (1);
 }
 
 1;
