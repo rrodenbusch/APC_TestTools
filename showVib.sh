9bda7e69f1467d702e5fe2e58aa88a912ae5bd7a
rrodenbusch <rrodenbusch@gmail.com>
2019-06-29 14:30:11 -0400

    fix y

--- showVib.sh
+++ showVib.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/pi/APC_TestTools/plotMPU.pl $1 2>//dev/null | feedgnuplot --domain --nodataid --stream --xlen 10000 -line --nopoint --ymax $1 --title "Vibration Gs"
+/home/pi/APC_TestTools/plotMPU.pl $1 2>//dev/null | feedgnuplot --domain --nodataid --stream --xlen 10000 -line --nopoint --ymax 10 --ymin -10 --title "Vibration Gs"
