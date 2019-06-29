3da7f58d3db4a05818d7609f85e49e12eadb9a67
rrodenbusch <rrodenbusch@gmail.com>
2019-06-29 10:25:51 -0500

    select data field output

--- plotDB.pl
+++ plotDB.pl
@@ -2,6 +2,16 @@
 use strict;
 use warnings;
 
+my %options;
+use Getopt::Std;
+getopts("pam",%options);
+my @mykeys = keys(%options);
+if ($#mykeys == -1 ) {
+	$options{p} = 1;
+	$options{a} = 1;
+	$options{m} = 1;
+}
+
 my $cmd = 'ssh pi@192.168.0.221 "cd /home/pi/I2C/i2cdata/dB;/home/pi/APC_TestTools/tailCSV.sh"';
 open(my $fh, "$cmd |");
 my $cnt = 0;
@@ -20,7 +30,10 @@ while ($line = <$fh>) {
 	my $epoch = shift(@fields);
 	my $msec = shift(@fields);
  	my $x = 1000*($epoch - $startTime) + int($msec/1000);
-	my $line = "$x ".join(' ',@fields);
+	my $line = "$x";
+	$line .= " $fields[0] $fields[1]" if (defined($options{p}));
+	$line .= " $fields[2] $fields[3]" if (defined($options{a}));
+	$line .= " $fields[4] $fields[5]" if (defined($options{m}));
 	print "$line\n"; 
 }
 
