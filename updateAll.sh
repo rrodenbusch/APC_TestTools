32fde404de9213d8322d118c176d8872e58b88e5
rrodenbusch <rrodenbusch@gmail.com>
2019-07-11 09:53:33 -0500

    Refresh all git

---
+++ updateAll.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ -d ~/updates/PiInstall ]
+   echo "Update PiInstall"
+   cd ~/updates/PiInstall
+   git pull origin master
+fi
+if [ -d ~/updates/RPi ]
+then
+   echo "Update RPi"
+   cd ~/updates/RPi
+   git pull origin master
+fi
+if [ -d ~/APC_TestTools ]
+then
+   echo "Update APC_TestTools"
+   cd ~/APC_TestTools
+   git pull origin master
+fi
+if [ -d ~/Arduino ]
+then
+   echo "Update Arduino"
+   cd ~/Arduiono
+   git pull origin master
+fi
+if [ -d ~/updates/NUC ]
+then
+   echo "Update NU"
+   cd /updates/NUC
+   git pull origin master
+fi
