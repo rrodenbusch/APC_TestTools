653026bf1268efa4e0720de29041a435a730b43c
rrodenbusch <rrodenbusch@gmail.com>
2019-07-12 17:28:11 -0500

    find pid

--- stopRPi.pl
+++ stopRPi.pl
@@ -2,15 +2,20 @@
 use strict;
 use warnings;
 
-
-my $pid;
 my @pidList;
 my @ProcList = ('SystemMonitor.pl','MapNetwork.pl','WatchDog.pl','ConfigServer.pl','ShutdownNUC.pl','i2c_apc_v6.pl',
 				'RemoteLog.pl', 'RunNVR.pl', 'RemoteData.pl','WatchSSD.pl','i2c_apc_v6.pl','Watcher.pl');
 				
 foreach my $curProc (@ProcList) {
-	$pid = `ps -C $curProc -o pid`;
-	push(@pidList,$pid);
+	my $return = `ps -C $curProc -o pid`;
+ 	my @lines = split("\n",$return);
+ 	my $pid = -1;
+ 	if ( scalar @lines > 1) {
+		my @fields = split(" ",$lines[0]);
+ 		$pid = $lines[1];
+ 		$pid =~ s/ //g;	
+ 	}
+	push(@pidList,$pid) unless $pid == -1;
 }
 my $cmd = "kill " . join(",",@pidList);
 `$cmd`;
