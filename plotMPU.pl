8872807bc3a2c8bad61a7c1ad0a8dced1af0e94f
rrodenbusch <rrodenbusch@gmail.com>
2019-07-20 17:05:06 -0500

    fix plotsO

--- plotMPU.pl
+++ plotMPU.pl
@@ -18,7 +18,7 @@ if ($#mykeys == -1 ) {
 }
 my $ip = 221;
 $ip = $ARGV[0] if defined($ARGV[0]);
-my $cmd = 'ssh pi@192.168.0.221 "cd /home/pi/I2C/i2cdata/MPU;/home/pi/APC_TestTools/tailCSV"';
+my $cmd = 'ssh pi@192.168.0.' . "$ip " . '"cd /home/pi/I2C/i2cdata/MPU;/home/pi/APC_TestTools/tailCSV"';
 open(my $fh, "$cmd |");
 my $cnt = 0;
 
