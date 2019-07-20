462f68fe978a5297b29b79ee45ecd2b7bfde7c81
rrodenbusch <rrodenbusch@gmail.com>
2019-07-20 16:48:53 -0500

    command line

--- showAvgDB.sh
+++ showAvgDB.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/pi/APC_TestTools/plotDB.pl -a 2>//dev/null | feedgnuplot --domain --nodataid --stream --xlen 10000 -line --nopoint --ymax $1 --title "Peak $1"
+/home/pi/APC_TestTools/plotDB.pl $1 -a 2>//dev/null | feedgnuplot --domain --nodataid --stream --xlen 10000 -line --nopoint --ymax $1 --title "Peak $2"
--- showDB.sh
+++ showDB.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/pi/APC_TestTools/plotDB.pl | feedgnuplot --domain --nodataid --stream --xlen 10000 -line --nopoint --ymax $1
+/home/pi/APC_TestTools/plotDB.pl $1 | feedgnuplot --domain --nodataid --stream --xlen 10000 -line --nopoint --ymax $2
