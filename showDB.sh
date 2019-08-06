e722ba1b4cda9b309e4cd758e1a23f9968484eaa
rrodenbusch <rrodenbusch@gmail.com>
2019-08-06 07:19:33 +0000

    change to home via env var

--- showDB.sh
+++ showDB.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/pi/APC_TestTools/plotDB.pl $1 | feedgnuplot --domain --nodataid --stream --xlen 10000 -line --nopoint --ymax $2
+$HOME/APC_TestTools/plotDB.pl $1 | feedgnuplot --domain --nodataid --stream --xlen 10000 -line --nopoint --ymax $2
