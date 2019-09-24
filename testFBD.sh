344825f2347e22e2d50113ebfc3159e260611ff0
rrodenbusch <rrodenbusch@gmail.com>
2019-09-24 20:27:57 +0000

    updated pull

--- testFBD.sh
+++ testFBD.sh
@@ -9,8 +9,14 @@ rm ./MPU/*
 rm ./temp/*
 rm ./voltage/*
 /home/pi/APC_TestTools/startPi
-/home/pi/APC_TestTools/queryI2C.pl
+gpio -g mode 18 out
+gpio -g write 18 0
+gpio -g mode 18 in
+gpio -g mode 23 out
+gpio -g write 23 0
+gpio -g mode 23 in
 sleep 4
+/home/pi/APC_TestTools/queryI2C.pl
 ########################
 /home/pi/APC_TestTools/i2c_1WireAddy.pl
 echo check Doors  Enter to Continue
@@ -22,4 +28,8 @@ echo check Temp Enter to Continue
 /home/pi/APC_TestTools/rtailCSV 221 temp
 echo check cams  Enter to Continue
 /home/pi/APC_TestTools/rtailCSV 231 png
+gpio -g mode 23 out
+gpio -g write 23 1
+gpio -g mode 18 out
+gpio -g write 18 1
 
