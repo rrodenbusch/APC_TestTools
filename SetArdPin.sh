089b514389f0824a197a51a6943040562db6366b
rrodenbusch <rrodenbusch@gmail.com>
2019-09-25 23:06:03 +0000

    fix

--- SetArdPin.sh
+++ SetArdPin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /home/pi/APC_TestTools/I2Cio.pl write0 0x0A 0x70  $1
-/home/pi/APC_TestTools/I2Cio.pl write0 0x0A 0x71 0xE1
+/home/pi/APC_TestTools/I2Cio.pl write0 0x0A 0x71  $2
 /home/pi/APC_TestTools/I2Cio.pl write0 0x0A 0x72 0xE1
 /home/pi/APC_TestTools/I2Cio.pl write0 0x0A 0x73 0xE1
 /home/pi/APC_TestTools/I2Cio.pl write0 0x0A 0x74 0x47
