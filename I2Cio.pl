1403d8182559b479b349eff3fe9e28f34ce422b4
rrodenbusch <rrodenbusch@gmail.com>
2019-09-25 17:06:23 -0500

    fix

--- I2Cio.pl
+++ I2Cio.pl
@@ -113,7 +113,12 @@ if (defined($cmd) && ($cmd eq 'read')) {
 	}	
 } elsif (defined($cmd) && ($cmd eq 'readword')) {
 	if ($device = attach($addy)) {
-		my $word1 = $device->read_word($register);
+		sleep(1);
+		my $word1;
+		while ( ($word1 = $device->read_word($register) == -1) {
+			"Read Error\n";
+			sleep(1);	
+		}
 		my $str = sprintf("%04X %06d %16b" ,$word1 & 0xFFFF,
 				$word1 & 0xFFFF, $word1 & 0xFFFF );
 		print "Word $word1 :: $str\n";
