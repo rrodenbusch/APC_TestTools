16990de5ad053047b464ab4ec75c34dba4f1c0e3
rrodenbusch <rrodenbusch@gmail.com>
2019-09-25 16:19:37 -0500

    fix

--- I2Cio.pl
+++ I2Cio.pl
@@ -160,6 +160,8 @@ if (defined($cmd) && ($cmd eq 'read')) {
 		warn "Device $addy NOT READY\n" unless ($device = attach($addy));
 	}	
 } elsif (defined($cmd) && ($cmd eq 'write0') ) {
+	my $str = sprintf(" %04X\n",$data);
+	print $str;
 	if ($device = attach($addy)) {
 		sleep(1);
 #		while (!$device->check_device($addy)) {
