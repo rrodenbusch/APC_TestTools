d448d69a65d532ffbd2c7cfb0cb55bc2289ef055
rrodenbusch <rrodenbusch@gmail.com>
2019-09-26 22:37:09 +0000

    updated test

--- testAPC.sh
+++ testAPC.sh
@@ -1,32 +1,35 @@
 /home/pi/APC_TestTools/BridgeOn.pl
 #!/bin/bash
 /home/pi/APC_TestTools/queryI2C.pl
-echo "Enter to continue"
-read var
-/home/pi/APC_TestTools/i2c_testMPU.pl
+echo "run i2c_testMPU.pl and return"
 echo "Enter to continue"
 read var
 /home/pi/APC_TestTools/BridgeOff.sh
 echo "Bridge Off: Enter to continue"
 read var
-/home/pi/APC_TestTools/BridgeOn.pl
+/home/pi/APC_TestTools/BridgeOn.sh
 echo "Bridge On: Enter to continue"
 gpio -g mode 18 out
 gpio -g mode 23 out
 gpio -g write 18 1
-gpio -g wire 23 1
+gpio -g write 23 1
 echo "Cams & Sensors off: Enter to continue"
 gpio -g write 23 0
 gpio -g write  18 0
 gpio -g mode 23 in 
 gpio -g mode 18 in
 echo "Enter to continue"
-gpio -g mode 19 in
+read var
+gpio -g mode 19 out
+gpio -g write 19 0
 echo "NVN off: Enter to continue"
 read var
-gpio -g mode 16 in
-gpio -g mode 26 in
-echo "NVN  off: Enter to continue (TNET will bounce)"
+gpio -g mode 19 in
+gpio -g mode 16 out
+gpio -g write 16 0
+gpio -g mode 26 out
+gpio -g write 26 0
+echo "NUC  off: Enter to continue (TNET will bounce)"
 read var
 gpio -g mode 12 out
 gpio -g write 12 1
