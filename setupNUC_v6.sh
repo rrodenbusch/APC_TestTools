57d5ba7226414268b2308b46c65ae4e9dd57efa0
rrodenbusch <rrodenbusch@gmail.com>
2019-11-09 17:53:34 +0000

    pre-setup

---
+++ setupNUC_v6.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "Setup the VISUDO"
+sudo visudo
+if [ -e /var/lib/dpkg/lock-frontend ];
+then
+   sudo rm /var/lib/dpkg/lock-frontend
+if
+if [ -e /var/lib/dpkg/lock ];
+then
+   sudo rm /var/lib/dpkg/lock
+fi
+echo "Install git"
+sudo apt-get install git
+echo "Updated repositories"
+if [ ! -d /home/mthinx/NUC ]; then
+   cd /home/mthinx
+   git clone https://github.com/rrodenbusch/NUC
+fi
+cd /home/mthinx/NUC
+git pull origin master
+if [ ! -d /home/mthinx/APC_TestTools ]; then
+   cd /home/mthinx
+   git clone https://github.com/rrodenbusch/APC_TestTools
+fi
+cd /home/mthinx/APC_TestTools
+git pull origin master
+
+cp /home/mthinx/NUC/authorized_key ~/.ssh
+
