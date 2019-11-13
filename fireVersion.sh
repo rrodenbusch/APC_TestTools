d8a365191ac189337eeb7c533f9aee5e2a669cdd
rrodenbusch <rrodenbusch@gmail.com>
2019-11-13 19:38:45 +0000

    OpenVPN

--- fireVersion.sh
+++ fireVersion.sh
@@ -2,4 +2,5 @@
 FIREW=`cat /opt/firewatchdog/version.txt`
 grep app.version  /opt/fire/Base.properties
 echo watchdog.version=$FIREW
+/opt/fire/openvpn/openvpn --version | grep Open | grep -v Copy
 
