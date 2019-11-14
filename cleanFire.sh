6b55082b14024b45188dde68a7c72b12ade892df
rrodenbusch <rrodenbusch@gmail.com>
2019-11-14 15:48:32 +0000

    fix

--- cleanFire.sh
+++ cleanFire.sh
@@ -2,7 +2,7 @@
 sudo service fire stop
 sudo rm -r /var/fire/config/openvpn/*
 sudo rmdir /var/fire/config/openvpn
-sudo rm /var/fire/config/*
+sudo rm -r /var/fire/config/*
 sudo systemctl stop fbd-doortemp
 sudo systemctl stop fbd-sound
 sudo systemctl stop fbd-mpu
