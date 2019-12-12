ee90747711a1dc33a43c436a6b43fbc3320dc041
rrodenbusch <rrodenbusch@gmail.com>
2019-12-12 08:45:21 -0600

    updated

--- i2c_testMPU.pl
+++ i2c_testMPU.pl
@@ -13,41 +13,46 @@ use lib "$ENV{HOME}/RPi";
 use warnings;
 use strict;
 use Time::HiRes;
-use RPiConfig;
+#use RPiConfig;
 use MPU6050;
 
-sub processSignals{
-	my $config = shift;
-	if ( $config->getSig('HUP')) {
- 		$config->resetSig('HUP');
- 		print("testMPU, HUP received");
- 	}	
- 	if ( $config->getSig('USR1')) {
- 		$config->resetSig('USR1');
- 		print("testMPU, USR1 received");
- 	}
- 	if ( $config->getSig('USR2')) {
- 		$config->resetSig('USR2');
- 		print("testMPU, USR2 received");
- 	}
-}	#	processSignals
+#sub processSignals{
+#	my $config = shift;
+#	if ( $config->getSig('HUP')) {
+# 		$config->resetSig('HUP');
+# 		print("testMPU, HUP received");
+# 	}	
+# 	if ( $config->getSig('USR1')) {
+# 		$config->resetSig('USR1');
+# 		print("testMPU, USR1 received");
+# 	}
+# 	if ( $config->getSig('USR2')) {
+# 		$config->resetSig('USR2');
+# 		print("testMPU, USR2 received");
+# 	}
+# 	if ( $config->getSig(INT)) {
+# 		$config->resetSig(INT);
+# 		print("testMPU, INT received");
+# 	}
+#}	#	processSignals
 
 
 my $deviceAddress = 0x68;
 my $device = MPU6050->new($deviceAddress);
-my $config = RPiConfig->new();
+#my $config = RPiConfig->new();
 my ($xCal,$yCal,$zCal) = (1.0,1.0,1.0);
-$xCal = $config->{xCal} if defined($config->{xCal});
-$yCal = $config->{yCal} if defined($config->{yCal});
-$zCal = $config->{zCal} if defined($config->{zCal});
-$config->{maxG} = 4 unless (defined($config->{maxG}));
-$config->{I2C_sleep} = 1 unless defined($config->{I2C_sleep});
-$device->wakeMPU($config->{maxG});
+#$xCal = $config->{xCal} if defined($config->{xCal});
+#$yCal = $config->{yCal} if defined($config->{yCal});
+#$zCal = $config->{zCal} if defined($config->{zCal});
+#$config->{maxG} = 4 unless (defined($config->{maxG}));
+#$config->{I2C_sleep} = 1 unless defined($config->{I2C_sleep});
+$device->wakeMPU(4);
 print "Waking MPU at maxG = $config->{maxG}\n";
 sleep(2);
-my $loopDelay = $config->{I2C_sleep} * 1000 * 1000;
-while( $config->getSig('ABRT') == 0) {
-	processSignals($config);
+my $loopDelay = 0.01 * 1000 * 1000;
+while( ($config->getSig('ABRT') == 0) &&
+       ($config->getSig('INT') ) ) {
+#	processSignals($cnfig);
 	my ($epoch,$msec) = Time::HiRes::gettimeofday();
 	my ($AcX,$AcY,$AcZ) = $device->readAccelG();
 	my ($tmp,$tmpC,$tmpF) = $device->readTemp();
