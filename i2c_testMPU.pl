d1b490d743f805d1e8805f1ead677bd56a56ef10
rrodenbusch <rrodenbusch@gmail.com>
2019-12-12 08:47:40 -0600

    updated

--- i2c_testMPU.pl
+++ i2c_testMPU.pl
@@ -16,6 +16,18 @@ use Time::HiRes;
 #use RPiConfig;
 use MPU6050;
 
+ Setup Signals
+my %sig = ( HUP => 0, ABRT => 0, USR1 => 0, USR2 => 0, CONT => 0, QUIT => 0, STOP => 0, INT => 0, SUSP => 0 );
+$SIG{HUP}  = sub {$sig{HUP} = 1};
+$SIG{ABRT} = sub {$sig{ABRT} = 1};
+$SIG{USR1} = sub {$sig{USR1} = 1};
+$SIG{USR2} = sub {$sig{USR2} = 1};
+$SIG{CONT} = sub {$sig{CONT} = 1};
+$SIG{QUIT} = sub {$sig{QUIT} = 1};
+$SIG{STOP} = sub {$sig{STOP} = 1};
+$SIG{INT}  = sub {$sig{INT} = 1};
+$SIG{SUSP} = sub {$sig{SUSP} = 1};
+
 #sub processSignals{
 #	my $config = shift;
 #	if ( $config->getSig('HUP')) {
@@ -47,11 +59,11 @@ my ($xCal,$yCal,$zCal) = (1.0,1.0,1.0);
 #$config->{maxG} = 4 unless (defined($config->{maxG}));
 #$config->{I2C_sleep} = 1 unless defined($config->{I2C_sleep});
 $device->wakeMPU(4);
-print "Waking MPU at maxG = $config->{maxG}\n";
+print "Waking MPU at maxG = 4\n";
 sleep(2);
 my $loopDelay = 0.01 * 1000 * 1000;
-while( ($config->getSig('ABRT') == 0) &&
-       ($config->getSig('INT') ) ) {
+while( ($sig{INT}==0) && ($sig{QUIT} == 0) &&
+       ($sig{STOP} == 0) ) {
 #	processSignals($cnfig);
 	my ($epoch,$msec) = Time::HiRes::gettimeofday();
 	my ($AcX,$AcY,$AcZ) = $device->readAccelG();
@@ -65,5 +77,9 @@ while( ($config->getSig('ABRT') == 0) &&
 	print "$line\n";
 	Time::HiRes::usleep($loopDelay);
 }
+
+print "Exit on Interrupt\n" if ($sig{INT} != 0);
+print "Exit on Stop\n" if ($sig{STOP} != 0);
+print "Exit on Quit\n" if ($sig{QUIT} != 0);
 
 1;
