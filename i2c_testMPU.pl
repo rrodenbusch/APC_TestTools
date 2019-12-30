0506115079887eb6862bac715182bbe712a6a544
rrodenbusch <rrodenbusch@gmail.com>
2019-12-30 08:14:29 -0600

    fix

--- i2c_testMPU.pl
+++ i2c_testMPU.pl
@@ -34,21 +34,30 @@ my ($xCal,$yCal,$zCal) = (1.0,1.0,1.0);
 $device->wakeMPU(4);
 print "Waking MPU at maxG = 4\n";
 sleep(2);
+my $errCnt = 0;
 my $loopDelay = 0.01 * 1000 * 1000;
 while( ($sig{INT}==0) && ($sig{QUIT} == 0) &&
        ($sig{STOP} == 0) ) {
    my ($epoch,$msec) = Time::HiRes::gettimeofday();
    my ($AcX,$AcY,$AcZ) = $device->readAccelG();
    my ($tmp,$tmpC,$tmpF) = $device->readTemp();
+   my $curErr = 0;
+   if ( ($AcX == -1) || ($AcY == -1) || ($AcZ == -1) || ($tmp == -1) ) {
+      $curErr = -1;
+      $errCnt++ if ($AcX == -1);
+      $errCnt++ if ($AcY == -1);
+      $errCnt++ if ($AcZ == -1);
+      $errCnt++ if ($tmp == -1);
+   }
    $AcX *= $xCal;
    $AcY *= $yCal;
    $AcZ *= $zCal;
    my $totG = sqrt($AcX*$AcX+$AcY*$AcY+$AcZ*$AcZ);
-   my $line= sprintf("%d,%6d,%04.3f,%04.3f,%04.3f,%04.3f,%d,%04.2f,%04.2f",$epoch,$msec,$totG,$AcX,$AcY,$AcZ,$tmp,$tmpC,$tmpF);
+   my $line= sprintf("%d,%6d,%04.3f,%04.3f,%04.3f,%04.3f,%d,%04.2f,%04.2f",$epoch,$msec,$totG,$AcX,$AcY,$AcZ,$tmp,$tmpC,$tmpF,$curErr);
    print "$line\n";
    Time::HiRes::usleep($loopDelay);
 }
-
+print "Error Count $errCnt\n";
 print "\nExit on Interrupt\n" if ($sig{INT} != 0);
 print "\nExit on Stop\n" if ($sig{STOP} != 0);
 print "\nExit on Quit\n" if ($sig{QUIT} != 0);
