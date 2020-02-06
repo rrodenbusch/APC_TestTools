b0d99be57200c5cb164bc88c83ec5c6f5f018c73
rrodenbusch <rrodenbusch@gmail.com>
2020-02-06 13:48:21 -0600

    calib2

--- MPUcalib.pl
+++ MPUcalib.pl
@@ -39,6 +39,8 @@ sleep(1);
 my $loopDelay = $delaySec * 1000 * 1000;
 my ($errCnt,$curErr,$tmpErr,$accErr,$samples) = (0,0,0,0,0);
 my ($StartEpoch,$StartMsec) = Time::HiRes::gettimeofday();
+my ($totX,$totY,$totZ) = (0,0,0);
+my $goodCnt=0;
 while( ($sig{INT}==0) && ($sig{QUIT} == 0) &&
        ($sig{STOP} == 0) && ($samples < 1000) ) {
    $curErr = 0;
@@ -56,6 +58,12 @@ while( ($sig{INT}==0) && ($sig{QUIT} == 0) &&
       $errCnt++ if ($AcZ == -1);
       $errCnt++ if ($tmp == -1);
    }
+   if ( ($AcX != -1) && ($AcY != -1) && ($AcZ != -1) ) {
+      $goodCnt++;
+      $totX += $AcX;
+      $totY += $AcY;
+      $totA += $AcZ;
+   }
  #  $AcX *= $xCal;
  #  $AcY *= $yCal;
  #  $AcZ *= $zCal;
@@ -65,6 +73,13 @@ while( ($sig{INT}==0) && ($sig{QUIT} == 0) &&
    print "$line\n";
    Time::HiRes::usleep($loopDelay);
 }
+my ($xMPU,$yMPU,$zMPU) = (0,0,0);
+if ($goodCnt > 0) {
+   $xMPU = -1 *$totX / $goodCnt;
+   $yMPU = -1 * $totY / $goodCnt;
+   $zMPU = -8192 + $totZ / $goodCnt;
+}
+
 my ($EndEpoch,$EndMsec) = Time::HiRes::gettimeofday();
 my $SampleTime = ($EndEpoch - $StartEpoch) + ($EndMsec - $StartMsec) / 1000000;
 my $SampleRate = $samples/$SampleTime;
@@ -73,6 +88,8 @@ print "\nExit on Stop\n" if ($sig{STOP} != 0);
 print "\nExit on Quit\n" if ($sig{QUIT} != 0);
 my $line = sprintf("Count %d Rate %6.2f Time %6.2f\n",$samples,$SampleRate,$SampleTime); 
 print "Errs(Tot TmpErr AccErr) $errCnt $tmpErr $accErr\n";
+print $line;
+$line = sprintf("Offsets %6.2f %6.2f %6.2f\n", $xMPU, $yMPU, $zMPU);
 print $line;
 
 1;
