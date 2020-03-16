af35257c99ddc6b9e52c3db64928706431120b64
rrodenbusch <rrodenbusch@gmail.com>
2020-03-16 10:54:59 -0500

    fix

--- getClip.pl
+++ getClip.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use Cwd qw(cwd);
+use Time::Local;
 
 
 use Getopt::Std;
@@ -38,17 +39,35 @@ if (scalar @ARGV > 0) { # No command line files
 my $fCnt = scalar @$fList;
 print "Looking for $startEpoch to $endEpoch in $fCnt files mac $MACs\n";
 
+my ($firstFile,$lastFile,@fullFiles);
 foreach my $curFile (@$fList) {
    my $dateStr = substr($curFile,0,-4);
    $dateStr = substr($dateStr,7);
    my @parts = split('-',$dateStr);
-   my $day =  substr($parts[0],0,8);
+   my $dayStr =  substr($parts[0],0,8);
    my $tofday = substr($parts[0],9,6);
    my $start = $parts[1];
    my $end = $parts[2];
+   my ($year,$mon,$mday) = (substr($dayStr,0,4),substr($dayStr,4,2),substr($dayStr,6,2));
+   my ($hh,$mm,$ss) = (substr($tofday,0,2),substr($tofday,2,2),substr($tofday,4,2));
+   $mon--;
    
-   print "Checking files\n";
+   my $fEpoch = timelocal($ss,$mm,$hh,$mday,$mon,$year);
+   my $fStartEpoch = $fEpoch + $start;
+   my $fEndEpoch = $fEpoch + $end;
+
+   my $firstFile = $curFile if ( ($startEpoch >= $fStartEpoch) && ($startEpoch <= $fEndEpoch) );
    
+   my $lastFile  = $curFile if ( ($endEpoch >= $fStartEpoch) && ($endEpoch <= $fEndEpoch) );
+   my (@fullFiles);
+   push(@fullFiles,$curFile) if ( ($fStartEpoch >= $startEpoch) && ($fEndEpoch <= $endEpoch) );
 }
+   
+   my $lineStr = "$firstFile ";
+   $lineStr .=  join(' ',@fullFiles) if (scalar @fullFiles > 0);
+   $lineStr .= " $lastFile";
+   
+   print "$lineStr\n";
+
 
 1;
