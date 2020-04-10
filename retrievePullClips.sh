feb6d3fe5f629680fad4a2e3495e4b892a314a51
rrodenbusch <rrodenbusch@gmail.com>
2020-04-10 11:32:48 -0500

    create scripts dirs

--- retrievePullClips.sh
+++ retrievePullClips.sh
@@ -52,6 +52,7 @@ cd $VDIR
 cd $DATE
 [[ -d clips ]] || mkdir clips
 cd clips
+[[ -d scripts ]] || mkdir scripts
 `rsync -e "ssh -i ~/PEM/richard-processing.pem" ubuntu@mbta-temp-flowz-server.mthinx.com:/home/ubuntu/MBTA/Working/$DATE/TripCoaches.csv .`
 `rsync -e "ssh -i ~/PEM/richard-processing.pem" ubuntu@mbta-temp-flowz-server.mthinx.com:/home/ubuntu/MBTA/Working/$DATE/CoachTrips.csv .`
 `rsync -e "ssh -i ~/PEM/richard-processing.pem" ubuntu@mbta-temp-flowz-server.mthinx.com:/home/ubuntu/MBTA/Working/$DATE/DoorEventData.$DATE.csv .`
