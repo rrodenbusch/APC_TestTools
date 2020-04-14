e749de14a10daa038b2d172ccadc2ea4a6f1b7c7
rrodenbusch <rrodenbusch@gmail.com>
2020-04-14 06:04:11 -0500

    3am

--- getTripDate.pl
+++ getTripDate.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 
 my $epoch = time();
-$epoch -= 7*3600;  # backup 7 hours of gmtime to get 2am eastern
+$epoch -= 8*3600;  # backup 7 hours of gmtime to get 3am eastern (1 hr after reports shift to next day)
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime($epoch);
 my $dateStr = sprintf("%04d%02d%02d",$year+1900,$mon+1,$mday);
 print "$dateStr\n";
