6cad344a4ed2466dbda2f6c20f0e0a8a4ddd0b46
rrodenbusch <rrodenbusch@gmail.com>
2020-05-06 09:14:31 -0500

    new

---
+++ systemCheck.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub cronCheck {
+   my %required = { 'openRTSP' => '0 7 * * * /usr/bin/killall openRTPS'};
+   my %matched;
+   my $newfile = 0;
+   my @newLines;
+  
+   my $cronfile = `crontab -l`;
+   my @cronlines = split('\n',$cronfile);
+   foreach my $curLine (@cronlines) {
+      $curLine =~ s/\R//g;
+      push (@newLines, $curLine) if ($curLine =~ m/~\s#/);
+      next if ($curLine =~ m/~\s#/);
+      foreach my $key (keys(%required)) {
+         if (substr($curLine,$key) != -1) {
+            $matched{$key} = 1;
+            if ($required{$key} ne $curLine) {
+               $newfile = 1;
+               $curLine = $required{$key};
+               next;
+            }
+         }
+      }  # next key
+      push(@newLines,$curLine);      
+   }  # next line of file
+   
+   if ($newfile) {
+      my $newLines = join("\n",@newLines);
+      `crontab -l $ENV{HOME}/RPi/cron.bak`;
+      my $fname = "$ENV{HOME}/RPi/cron.new";
+      open(my $fh, ">$fname") or die "Unable to open $fname\t$!\n";
+      print $fh "$newLines\n";
+      close $fh;
+      `crontab $fname`;
+   }
+
+}
+
+cronCheck();
+
+
+
+
+
+1;
