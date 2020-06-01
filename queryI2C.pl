0b05e49f337336f8c28d3b6610ed862d7edf467b
rrodenbusch <rrodenbusch@gmail.com>
2020-06-01 13:53:41 -0500

    dowtakes too long to dev now that pi r locked wn

--- queryI2C.pl
+++ queryI2C.pl
@@ -180,29 +180,26 @@ foreach $curAddy (@UIDs) {
    }
 }
 
-#my $PwrBit  = 0x01;
-#my $CapBit  = 0x02;
-#my $PiBit   = 0x04;
-#my $NUCBit  = 0x08;
-#my $TNetBit = 0x10;
-#my $BrdgBit = 0x20;
-#my $NVNBit  = 0x40;
-#my $SnsrBit = 0x80;
-
 foreach $curAddy (@GPIOs) {
    if ($ValidI2C[$curAddy] ) { # check the GPIOs
       print "\nChecking $DeviceNames->{$curAddy}\n";
       if ($device = attach($curAddy)) {
-         my ($byte1,$cnt1) = getI2CdataByte($device,0x00);
-         my ($byte2,$cnt2) = getI2CdataByte($device,0x09);
-         my ($byte3,$cnt3) = getI2CdataByte($device,0x0A); 
+         my ($byte1,$cnt1) = $device->read_byte(0x00);
+         my ($byte2,$cnt2) = $device->read_byte(0x09);
+         my ($byte3,$cnt3) = $device->read_byte(0x0A); 
          my $line= sprintf( "GPIO: IODIR: %08b OLAT: %08b GPIO: %08b\n",$byte1,$byte3,$byte2);
-         my ($bit1,$bit2,$bit3,$bit4,$bit5,$bit6,$bit7) = ( ($byte2 & 0x01),($byte2>>1 & 0x01),($byte2>>2 & 0x01)>,($byte2>>3 & 0x01),
-                                                            ($byte2>>4 & 0x01),($byte2>>5 & 0x01),($byte2>>6 & 0x01),($byte2>>7 & 0x01)   );
-         my $apcpwrstr = sprintf("Power Bits: Snsr %1x  NVN %1x Brdg %1x TNet %1x NUC %01x Pi %01x CapOk %01x PwrOk %01x\n",$bit7,$bit6,$bit5,$bit4,$bit3,$bit2,$bit1);
-         print $apcpwrstr if ( $curAddy} == 0x21 );
+         my ($bit0,$bit1,$bit2,$bit3,$bit4,$bit5,$bit6,$bit7 ) = ( ($byte2 & 0x01),     ($byte2 >> 1) & 0x01,($byte2 >> 2) & 0x01,($byte2 >>3) & 0x01,
+                                                                   ($byte2 >> 4) & 0x01,($byte2 >> 5) & 0x01,($byte2 >> 6) & 0x01,($byte2 >>7) & 0x01  );
+         my $apcpwrstr  = sprintf("APC Power  Bits: Snsr %01x  NVN %01x Brdg %01x TNet %01x NUC %01x Pi %01x CapOk %01x PwrOk %01x\n",$bit7,$bit6,$bit5,$bit4,$bit3,$bit2,$bit1,$bit0);
+         my $apcctlstr  = sprintf("APC Control Bits: Snsr %01x  NVN %01x Brdg %01x TNet %01x NUC %01x Pi %01x CapOk %01x PwrOk %01x\n",$bit7,$bit6,$bit5,$bit4,$bit3,$bit2,$bit1,$bit0);
+         my $fbdsnsstr  = sprintf("FBD Sense Bits: Snsr %01x  NVN %01x Brdg %01x TNet %01x NUC %01x Pi %01x CapOk %01x PwrOk %01x\n",$bit7,$bit6,$bit5,$bit4,$bit3,$bit2,$bit1,$bit0);
+         my $fbddiostr  = sprintf("FBD DIO Bits: Snsr %01x  NVN %01x Brdg %01x TNet %01x NUC %01x Pi %01x CapOk %01x PwrOk %01x\n",$bit7,$bit6,$bit5,$bit4,$bit3,$bit2,$bit1,$bit0);
          
          print $line;
+         print $apcpwrstr if ( $curAddy == 0x21 );
+         print $apcctlstr if ( $curAddy == 0x22 );
+         print $fbdsnsstr if ( $curAddy == 0x26 );
+         print $fbddiostr if ( $curAddy == 0x27 );
       } else {
          print "Error attaching to $curAddy\n";
       }
