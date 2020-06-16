7cd7c37b47b2aab5fb79c97dfee8672d53079eaa
rrodenbusch <rrodenbusch@gmail.com>
2020-06-16 09:34:49 -0500

    moving tools

--- BridgeOn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gpio -g write 24 0
-gpio -g mode 24 in
-/home/pi/APC_TestTools/I2Cio.pl write 0x22 0x00 0xFF
-/home/pi/APC_TestTools/I2Cio.pl write 0x22 0x0A 0x00
-
--- checkVicors.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while [ 1 ]; do
-   /home/pi/APC_TestTools/I2Cio.pl read 0x0A 0x0A
-   /home/pi/APC_TestTools/I2Cio.pl read 0x0A 0x0B
-   sleep 2
-done
