7103706d7f79bb3de5a804e7dca88047e3ab3dd3
rrodenbusch <rrodenbusch@gmail.com>
2020-06-21 19:20:36 +0100

    uptime

--- systemCheck.pl
+++ systemCheck.pl
@@ -123,7 +123,7 @@ my $config = readINI();
 my ($coach,$runDate) = @ARGV;
 print "\n\n$delim  System Check $config->{myRole} $coach $runDate $delim\n";
 
-my @cmdKeys = ( 'USAGE','PROCS','NVR','CLIPD','CLIPS');
+my @cmdKeys = ( 'USAGE','PROCS','NVR','CLIPD','CLIPS','UPTIME');
 my %Commands = ('USAGE'    => 'df -h | grep -v ^none | ( read header ; echo "$header" ; sort -rn -k 5)',
                 'PROCS'    => "$ENV{HOME}/APC_TestTools/stopRPi.pl",
                 'NVR'      => 'ls -ltr /data/NVR/Working |tail -10 |grep mp4',
