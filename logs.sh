d8a49bc4c53bf88a8c5980a0aef2bc42b21609b0
rrodenbusch <rrodenbusch@gmail.com>
2020-06-26 12:08:57 -0500

    fix

--- logs.sh
+++ logs.sh
@@ -4,12 +4,14 @@ lognames=('/home/pi/Watch.log','/home/pi/RPi/WatchDog.log','/home/pi/power.log',
 # rotate @ 200 Meg
 MaxFileSize=209715200
 movelog() {
-   if [ [ -e $1 ] ]; then
+   if [ -f $1 ]; then
       file_size=`du -b /script_logs/test.log | tr -s '\t' ' ' | cut -d' ' -f1`
+      echo "$1 file size $file_size vs Max $MaxFileSize"
       if [ $file_size -gt $MaxFileSize ]; then /usr/bin/savelog $1; fi
    fi
 }
 
-for fname in "${lognames[@]}"; do
+for fname in "${lognames[@]}"
+do
    movelog  $fname 
 done
