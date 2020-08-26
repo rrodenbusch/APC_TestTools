5d887e19bf30e26cbcd190d23aadcb7b3435af6c
rrodenbusch <rrodenbusch@gmail.com>
2020-08-26 11:10:03 +0000

    clean

--- cleanFire.sh
+++ cleanFire.sh
@@ -3,14 +3,8 @@ sudo service fire stop
 sudo rm -r /var/fire/config/openvpn/*
 sudo rmdir /var/fire/config/openvpn
 sudo rm -r /var/fire/config/*
-sudo systemctl stop fbd-doortemp
-sudo systemctl stop fbd-sound
-sudo systemctl stop fbd-mpu
-sudo systemctl disable fbd-doortemp
-sudo systemctl disable fbd-mpu
-sudo systemctl disable fbd-sound
-sudo rm /etc/systemd/system/fbd-*
-
+cd /var/fire
+rm -r *
 sudo ls -ltr /var/fire/config/openvpn
 sudo ls -ltr /var/fire/config
 
