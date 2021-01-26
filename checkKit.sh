4c4e20b5efcdb6a9fe6519e6570d081a9ac0dc92
rrodenbusch <rrodenbusch@gmail.com>
2021-01-26 19:46:26 +0000

    DistNameInfo

--- checkKit.sh
+++ checkKit.sh
@@ -2,4 +2,4 @@
 sudo apt-get -y remove pulseaudio
 sudo apt-get -y install fping
 sudo cpan install CPAN
-sudo cpan install Config::IniFiles RPi::Pin  RPi::Const Log::Log4perl
+sudo cpan install CPAN::DistnameInfo Config::IniFiles RPi::Pin  RPi::Const Log::Log4perl
