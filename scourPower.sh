5819ac8418af9bdec8601f4d67182bb6dfae26e5
rrodenbusch <rrodenbusch@gmail.com>
2021-02-11 14:51:26 -0600

    log

--- scourPower.sh
+++ scourPower.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $HOME
 $HOME/APC_TestTools/scourPower.pl >>scour.log 2>&1
-/usr/bin/rsync -r $HOME/B827EB* mthinx@$1:/extdata/power 2>& >>scour.log
+/usr/bin/rsync -r $HOME/B827EB* mthinx@$1:/extdata/power 2>&1 >>scour.log
 
