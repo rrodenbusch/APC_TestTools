baff55eb04e44ea714d517eef9c0cafad4f97904
rrodenbusch <rrodenbusch@gmail.com>
2021-02-11 15:03:04 -0600

    rsync

--- scourPower.sh
+++ scourPower.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/pi
 /home/pi/APC_TestTools/scourPower.pl >>scour.log 2>&1
-/usr/bin/rsync -r /home/pi/B827EB* mthinx@$1:/extdata/power 2>&1 >>scour.log
+/usr/bin/rsync -r /home/pi/B827EB* mthinx@$1:/extdata/power >>scour.log 2>&1
 
