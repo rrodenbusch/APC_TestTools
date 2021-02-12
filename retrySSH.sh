0520925aa051c9fd1e20d27ddcb2953588fe4572
rrodenbusch <rrodenbusch@gmail.com>
2021-02-12 07:44:41 -0600

    exits

--- retrySSH.sh
+++ retrySSH.sh
@@ -3,11 +3,16 @@
 [ -z $1 ] && echo 'retrySSH.sh user IP "cmd"' && exit 1
 [ -z $2 ] && echo 'retrySSH.sh user IP "cmd"' && exit 1
 while [ 1 ]; do
-   fping -c 1 -t 200 $2
+   fping -c 1 -t 200 -4 $2
    if [ "$?" == 0 ]; then
        ssh $1@$2  "$3"
-       [ "$?" == "0" ] && echo "Success" && sleep 1 && ssh $1@$2
+       if [ "$?" == "0" ]; then
+          echo "Success";
+          ssh $1@$2;
+          exit 0;
+       fi
    fi
    sleep 1
 done
 
+exit 1
