276124c79c660c5518604b1fb0aba506467ad47e
rrodenbusch <rrodenbusch@gmail.com>
2021-02-14 17:23:24 -0600

    two params

--- wrapPower.sh
+++ wrapPower.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/pi
-/home/pi/APC_TestTools/wrapPower.pl $1 >>scour.log 2>&1
+/home/pi/APC_TestTools/wrapPower.pl $2 >>scour.log 2>&1
 gzip /home/pi/B827EB*.csv
 /usr/bin/rsync -r /home/pi/B827EB* mthinx@$1:/extdata/power >>scour.log 2>&1
 
