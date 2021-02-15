dfef852e2fc7542e0a4704e3eed6a4a7b4cc9d9c
rrodenbusch <rrodenbusch@gmail.com>
2021-02-15 17:18:12 -0600

    fix stopR

--- stopRPi.pl
+++ stopRPi.pl
@@ -8,17 +8,15 @@ my @ProcList = ('SystemMonitor.pl','MapNetwork.pl','WatchDog.pl','ConfigServer.p
 				    'RemoteLog.pl', 'RunNVR.pl', 'RemoteData.pl','WatchSSD.pl','i2c_apc_v6.pl','Watcher.pl','WatchRPi.pl','i2c_fbd.pl');
 my $ret0 = `ps -elf |grep perl` ;
 print "\nBEFORE:\n$ret0\n";
+
 foreach my $curProc (@ProcList) {
 	my $return = `ps -eo pid,command |grep $curProc |grep -v grep`;
  	my @lines = split("\n",$return);
- 	my $pid = -1;
- 	if ( scalar @lines > 1) {
-		my @fields = split(" ",$lines[0]);
-		my $proc = pop(@fields);
- 		$pid = $lines[1];
- 		$pid =~ s/ //g;	
+   foreach my $curLine (@lines) {
+      my ($pid,$proc) = split(" ",$curLine);
+ 		$pid =~ s/ //g;
+      push(@pidList,$pid) if defined($pid);
  	}
-	push(@pidList,$pid) unless $pid == -1;
 }
 $ret0 = `ps -eopid,command |grep openRTSP |grep -v grep`;
 print "$ret0\n";
